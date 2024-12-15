4c940f5491a3d881fc918697691f99d155277c03
JeremKy <jeremky@me.com>
2024-12-15 21:14:49 +0100

    Passage des tabulations à 2

--- aptinstall.sh
+++ aptinstall.sh
@@ -8,20 +8,20 @@ list="$dir/$(basename -s .sh $0).lst"
 
 ## User
 if [ "$USER" != "root" ] ; then
-    echo "Droits root nécessaires"
-    exit 0
+  echo "Droits root nécessaires"
+  exit 0
 fi
 
 ## Vérification de la présence des fichiers
 if [ -f $cfg ] ; then
-    . $cfg
+  . $cfg
 else
-    echo "Fichier $cfg introuvable"
-    exit 0
+  echo "Fichier $cfg introuvable"
+  exit 0
 fi
 if [ ! -f $list ] ; then
-    echo "Fichier $list introuvable"
-    exit 0
+  echo "Fichier $list introuvable"
+  exit 0
 fi
 
 ## Installation des paquets
@@ -30,30 +30,30 @@ apt -y install $(cat $list | grep -v '#')
 
 ## Activation de locate
 if [ -f /usr/bin/locate ] ; then
-    updatedb
+  updatedb
 fi
 
 ## Activation des mises à jour automatiques
 if [ -f /usr/bin/unattended-upgrades ] ; then
-    dpkg-reconfigure unattended-upgrades
+  dpkg-reconfigure unattended-upgrades
 fi
 
 ## Sécurisation de ssh (check sur https://www.ssh-audit.com/#)
 if [ ! -f /etc/ssh/sshd_config.old ] ; then
-    cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
-    echo "" >> /etc/ssh/sshd_config 
-    echo "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers -chacha20-poly1305@openssh.com" >> /etc/ssh/sshd_config
-    systemctl restart sshd
+  cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
+  echo "" >> /etc/ssh/sshd_config 
+  echo "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers -chacha20-poly1305@openssh.com" >> /etc/ssh/sshd_config
+  systemctl restart sshd
 fi
 
 ## Activation du Firewall (avec désactivation de l'IP v6)
 if [ -f /usr/sbin/ufw ] ; then
-    for ufwallow in $ufwlist ; do
-        ufw allow $ufwallow
-    done
-    sed -i "s,IPV6=yes,IPV6=no," /etc/default/ufw
-    ## Ajout pour Podman
-    ufw allow in on podman1
-    ufw default allow FORWARD
-    ufw enable
+  for ufwallow in $ufwlist ; do
+    ufw allow $ufwallow
+  done
+  sed -i "s,IPV6=yes,IPV6=no," /etc/default/ufw
+  ## Ajout pour Podman
+  ufw allow in on podman1
+  ufw default allow FORWARD
+  ufw enable
 fi
