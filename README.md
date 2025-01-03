3a37c5a009ea4800cb93bcf81c8f5a3cf3d9cfba
JeremKy <jeremky@me.com>
2025-01-03 22:26:46 +0100

    Mise à jour

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # aptinstall.sh
 
-Script automatisant l'installation et le parametrage de Debian
+Script automatisant l'installation et le parametrage de Debian :
 
 - Installe les paquets présents dans le fichier aptinstall.lst
 
-- Sécurise le serveur SSH
+- Sécurise le serveur SSH et limite l'accès au user par défaut
 
 - Active le Firewall UFW si installé
 
 - Configure unattended-upgrades si installé
 
-- Active la commande locate si installé
+- Active la commande locate si installée
--- aptinstall.sh
+++ aptinstall.sh
@@ -1,32 +1,28 @@
 #!/bin/dash
 set -e
 
-## Variables
 dir=$(dirname "$0")
-cfg="$dir/$(basename -s .sh $0).cfg"
-list="$dir/$(basename -s .sh $0).lst"
 
-## User
+## Vérification des droits root
 if [ "$USER" != "root" ] ; then
   echo "Droits root nécessaires"
   exit 0
 fi
 
-## Vérification de la présence des fichiers
+## Vérification de la présence du fichier du config
 if [ -f $cfg ] ; then
   . $cfg
 else
   echo "Fichier $cfg introuvable"
   exit 0
 fi
-if [ ! -f $list ] ; then
-  echo "Fichier $list introuvable"
-  exit 0
-fi
 
 ## Installation des paquets
-apt update && apt -y dist-upgrade
-apt -y install $(cat $list | grep -v '#')
+apt update && apt -y full-upgrade
+list="$dir/aptinstall.lst"
+if [ -f $list ] ; then
+  apt -y install $(cat $list | grep -v '#')
+fi
 
 ## Activation de locate
 if [ -f /usr/bin/locate ] ; then
@@ -40,6 +36,7 @@ fi
 
 ## Sécurisation de ssh (check sur https://www.ssh-audit.com/#)
 if [ ! -f /etc/ssh/sshd_config.old ] ; then
+  user=$(id -un 1000)
   cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
   echo "" >> /etc/ssh/sshd_config 
   echo "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers -chacha20-poly1305@openssh.com" >> /etc/ssh/sshd_config
@@ -54,9 +51,7 @@ fi
 
 ## Activation du Firewall (avec désactivation de l'IP v6)
 if [ -f /usr/sbin/ufw ] ; then
-  for ufwallow in $ufwlist ; do
-    ufw allow $ufwallow
-  done
+  ufw allow ssh
   sed -i "s,IPV6=yes,IPV6=no," /etc/default/ufw
   ## Ajout pour Podman
   ufw allow in on podman1
