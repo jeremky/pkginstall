266e7b25fc15291a91cde80e4b07558a53c49ce1
JeremKy <jeremky@me.com>
2025-01-04 13:31:36 +0100

    Mise à jour

--- aptinstall.sh
+++ aptinstall.sh
@@ -9,14 +9,6 @@ if [ "$USER" != "root" ] ; then
   exit 0
 fi
 
-## Vérification de la présence du fichier du config
-if [ -f $cfg ] ; then
-  . $cfg
-else
-  echo "Fichier $cfg introuvable"
-  exit 0
-fi
-
 ## Installation des paquets
 apt update && apt -y full-upgrade
 list="$dir/aptinstall.lst"
@@ -36,10 +28,9 @@ fi
 
 ## Sécurisation de ssh (check sur https://www.ssh-audit.com/#)
 if [ ! -f /etc/ssh/sshd_config.old ] ; then
-  user=$(id -un 1000)
   cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
   echo "" >> /etc/ssh/sshd_config 
-  echo "# Secure Config\nX11Forwarding no\nAllowUsers $user\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers -chacha20-poly1305@openssh.com" >> /etc/ssh/sshd_config
+  echo "# Secure Config\nX11Forwarding no\nAllowUsers $(id -un 1000)\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512,hmac-sha2-256,hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers -chacha20-poly1305@openssh.com" >> /etc/ssh/sshd_config
   systemctl restart sshd
 fi
 
