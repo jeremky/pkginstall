f6ae4a6d8c4eaa6ee40af166e75e88f031d1f1da
JeremKy <jeremky@me.com>
2025-07-21 11:11:48 +0200

    Mise à jour

--- pkginstall.sh
+++ pkginstall.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -e
 
+# Messages en couleur
+error()    { echo -e "\033[0;31m====> $*\033[0m" ;}
+message()  { echo -e "\033[0;32m====> $*\033[0m" ;}
+warning()  { echo ; echo -e "\033[0;33m====> $*\033[0m" ;}
+
 # Vérification des droits root
 if [[ "$USER" != "root" ]]; then
-  echo "Droits root nécessaires"
+  error "Droits root nécessaires"
   exit 0
 fi
 
@@ -20,6 +25,7 @@ dist=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2,2)
 list="$(dirname "$0")/config/$dist.lst"
 
 # Installation des paquets
+warning "Installation des paquets..."
 case $dist in
   debian)
     apt update && apt -y full-upgrade
@@ -38,6 +44,7 @@ case $dist in
     fi
     ;;
 esac
+message "Paquets installés"
 
 # Activation de locate
 if [[ -f /usr/bin/locate ]]; then
@@ -46,10 +53,12 @@ fi
 
 # Sécurisation de ssh (check sur https://www.ssh-audit.com/#)
 if [[ ! -f /etc/ssh/sshd_config.old && -f /etc/ssh/sshd_config ]]; then
+  warning "Sécurisation de ssh..."
   cp -p /etc/ssh/sshd_config /etc/ssh/sshd_config.old
   echo "" >> /etc/ssh/sshd_config 
   echo -e "# Secure Config\nX11Forwarding no\nAllowUsers $(id -un 1000)\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-gcm@openssh.com,aes128-ctr" >> /etc/ssh/sshd_config
   systemctl restart sshd
+  message "ssh sécurisé"
 fi
 
 # Correction du bug fail2ban
@@ -60,6 +69,7 @@ fi
 
 # Activation du Firewall (avec désactivation de l'IP v6)
 if [[ -f /usr/sbin/ufw ]]; then
+  warning "Activation du firewall ufw..."
   if [[ $ipv6 = "false" ]]; then
     sed -i "s,IPV6=yes,IPV6=no," /etc/default/ufw
   fi
@@ -70,4 +80,5 @@ if [[ -f /usr/sbin/ufw ]]; then
     ufw allow $port
   done
   ufw enable
+  message "ufw activé"
 fi
