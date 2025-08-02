065aad5ff084ca2ab7af815cc6602a05a033560a
JeremKy <jeremky@me.com>
2025-08-02 12:01:09 +0200

    Mise à jour

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
-# gnomeconf.sh
+# pkginstall.sh
 
-Script automatisant le paramétrage de Gnome sur votre distribution préférée.
+Script automatisant l'installation et le paramétrage de Debian/Fedora.
+
+## Fonctionnalités
+
+- Installe les paquets présents dans le fichier `config/<votre_os>.lst` 
+
+- Sécurise le serveur SSH et limite l'accès à l'utilisateur par défaut
+
+- Active le firewall UFW sur debian si installé
+
+- Configure unattended-upgrades sur debian si installé
+
+- Active la commande locate si installée
 
 ## Configuration
 
-Un fichier de configuration `gnomeconf.cfg` est présent pour configurer les différents éléments selon vos préférences.
+Un fichier de configuration `pkginstall.cfg` est présent pour configurer les éléments du firewall UFW. Vous pouvez spécifier les ports à autoriser (séparés par des espaces), ainsi qu'indiquer si vous désirez désactiver l'ipv6. Le fichier de configuration : 
+
+```txt
+# pkginstall config
+ufwipv6=false
+ufwports="22/tcp 80/tcp 443"
+```
 
 ## Utilisation
 
-Pour exécuter le script : 
+Pour exécuter le script, vous devez disposer des droits root :
 
 ```bash
-./gnomeconf.sh
+sudo ./pkginstall.sh
 ```
---
+++ pkginstall.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -e
+
+# Messages en couleur
+error()    { echo -e "\033[0;31m====> $*\033[0m" ;}
+message()  { echo -e "\033[0;32m====> $*\033[0m" ;}
+warning()  { echo ; echo -e "\033[0;33m====> $*\033[0m" ;}
+
+# Vérification des droits root
+if [[ "$USER" != "root" ]]; then
+  error "Droits root nécessaires"
+  exit 0
+fi
+
+# Chargement du fichier de config
+cfg="$(dirname "$(realpath "$0")")/pkginstall.cfg"
+if [[ ! -f $cfg ]]; then
+  error "Fichier $cfg introuvable"
+  exit 1
+else
+  . $cfg
+fi
+
+# Config selon la distribution
+dist=$(cat /etc/os-release | grep "^ID=" | cut -d= -f2,2)
+list="$(dirname "$0")/config/$dist.lst"
+
+# Installation des paquets
+warning "Installation des paquets..."
+case $dist in
+  debian|ubuntu)
+    apt update && apt -y full-upgrade
+    if [[ -f $list ]]; then
+      apt -y install $(cat $list | grep -v '#')
+      # Activation des mises à jour automatiques
+      if [[ -f /usr/bin/unattended-upgrades ]]; then
+        dpkg-reconfigure unattended-upgrades
+      fi
+    fi
+    ;;
+  fedora)
+    dnf -y upgrade
+    if [[ -f $list ]]; then
+      dnf -y install $(cat $list | grep -v '#')
+    fi
+    ;;
+esac
+message "Paquets installés"
+
+# Activation de locate
+if [[ -f /usr/bin/locate ]]; then
+  updatedb
+fi
+
+# Sécurisation de ssh (check sur https://www.ssh-audit.com)
+if [[ -d /etc/ssh/sshd_config.d ]] && [[ ! -f /etc/ssh/sshd_config.d/$(id -un 1000).conf ]]; then
+  warning "Sécurisation de SSH..."
+  echo -e "# Secure Config\nX11Forwarding no\nAllowUsers $(id -un 1000)\nHostKey /etc/ssh/ssh_host_ed25519_key\nPasswordAuthentication yes\nKexAlgorithms curve25519-sha256@libssh.org\nMACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com\nCiphers aes256-gcm@openssh.com,aes256-ctr,aes192-ctr,aes128-gcm@openssh.com,aes128-ctr" > /etc/ssh/sshd_config.d/$(id -un 1000).conf
+  systemctl restart sshd
+  message "SSH sécurisé"
+fi
+
+# Correction du bug fail2ban
+if [[ -f /etc/fail2ban/jail.conf ]]; then
+  sed -i "s,backend = %(sshd_backend)s,backend = systemd," /etc/fail2ban/jail.conf
+  systemctl restart fail2ban
+fi
+
+# Activation du Firewall (avec désactivation de l'IP v6)
+if [[ -f /usr/sbin/ufw ]]; then
+  warning "Activation du firewall ufw..."
+  if [[ $ufwipv6 = "false" ]]; then
+    sed -i "s,IPV6=yes,IPV6=no," /etc/default/ufw
+  fi
+  if [[ -n $ufwports ]]; then
+    for port in $ufwports; do
+      ufw allow $port
+    done
+  fi
+  ufw enable
+  message "ufw activé"
+fi
